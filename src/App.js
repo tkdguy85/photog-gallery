248a3ad50f4d10fa075967e21264bf2abac7b860
tkdguy85 <dallas.gordon3@gmail.com>
2021-05-18 20:07:51 -0500

    Picture uploads disabled (pt3)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modals';
 import Title from './comps/Title';
-import Form from './comps/Uploads';
+// Temporarily disabled photo uploads.
+// import Form from './comps/Uploads';
 
 function App() {
 	const [selectedImg, setSelectedImg] = useState(null);
